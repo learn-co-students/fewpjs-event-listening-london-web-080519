a2111347cf6c0f9c550eae99ed833add10c3a7ad
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-09-16 10:11:21 +0100

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    //the node that is doing the listening
+    const input = document.getElementById('input');
+    // make it listen ---- what it's listening for
+    input.addEventListener('click', function(event) {
+        //what to do when it happens
+    alert('I was clicked!');
+    });
 }
